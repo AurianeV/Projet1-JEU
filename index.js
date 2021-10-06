7ea0845d506fb55ecac6054c8730740a56f1f69a
Auriane Viggiano <auriane.vigg@gmail.com>
2021-10-06 23:27:10 +0200

    debut js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const canvas = document.querySelector('#cassebrique');
+const ctx = canvas.getContext('2d');
 
+document.querySelector('#canvas').click(function () {
+    .divgeneral.hide();
+    #cassebrique.show();
+}); 
