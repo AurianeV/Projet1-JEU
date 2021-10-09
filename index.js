01c58d0b56fb5aeee0c5fae31978951cd3380e25
Auriane Viggiano <auriane.vigg@gmail.com>
2021-10-09 12:50:59 +0200

    debut js

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-const canvas = document.querySelector('#cassebrique');
-const ctx = canvas.getContext('2d');
 
-document.querySelector('#canvas').click(function () {
-    .divgeneral.hide();
-    #cassebrique.show();
-}); 
+var canvas = document.getElementById("cassebrique");
+var ctx = canvas.getContext("2d");
+
+  document.querySelector(".startbutton").onclick = function () {
+  // masquer divgeneral
+  document.querySelector(".divgeneral").style.display = "none";
+
+  // reveler canvas
+  document.querySelector("#cassebrique").style.display = "block";
+} 
+
+// Dessiner les briques 
+
+ctx.beginPath();
+ctx.rect(30, 50, 110, 30);
+ctx.fillStyle = "purple";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(100, 100, 110, 30);
+ctx.fillStyle = "purple";
+ctx.fill();
+ctx.closePath();
+
+
+// Dessiner la balle
+
+function draw() {
+
+    ctx.beginPath();
+    ctx.arc(500, 200, 18, 0, Math.PI*2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+    }
+
+draw();
+
+// Animer la balle 
+
+let speed1 = 0;
+let speed2 = 0;
+let speed3 = 0;
+
+function clearBall() {
+    ctx.clearRect(0, 0, 500, 200); 
+  }
