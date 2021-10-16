93ce833f2a3865cebd6f238e5d80df47e0e47449
Auriane Viggiano <auriane.vigg@gmail.com>
2021-10-16 13:24:54 +0200

    done

--- index.js
+++ index.js
@@ -16,28 +16,59 @@ document.querySelector(".startbutton").onclick = function () {
 
 
 const width = 110; // coordonnées largeur
-const heigth = 30; // coordonnées de hauteur
+const height = 30; // coordonnées de hauteur
 
 let plateformX = 665; // coordonée x de la plateforme
-let plateformY = 650; // coordonée y de la plateforme
+let plateformY = 660; // coordonée y de la plateforme
 let plateformWidth = 150; // largeur de la plateforme
-let plateformHeigth = 20; // hauteur de la plateforme
+let plateformHeigth = 23; // hauteur de la plateforme
 
 let diametreBall = 13;
 let Ballx = 700; // coordonnée x de la balle
 let Bally = 650; // coordonnée y de la balle
 
-let dx = 3; // mouvement de la balle sur l'axe des x
-let dy = -3; // mouvement de la balle  sur l'axe des y 
 
-class Brique {
+class Component {
+  constructor(){
+    this.on = true;
+  }
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+ 
+  crashWith(comp) {
+    // this / comp
+    return (
+      this.bottom() > comp.top() &&
+      this.top() < comp.bottom() &&
+      this.right() > comp.left() &&
+      this.left() < comp.right()
+    );
+  }
+}
+
+
+
+class Brique extends Component{
   constructor(x, y) {
+    super();
     this.x = x;
     this.y = y;
+    this.width = width
+    this.height = height
   }
   draw() {
     ctx.beginPath();
-    ctx.rect(this.x, this.y, width, heigth);
+    ctx.rect(this.x, this.y, this.width, this.height);
     ctx.createLinearGradient=(25, 25, 100, 25);
     ctx.fillStyle="#6C0277";
     ctx.fill();
@@ -45,11 +76,13 @@ class Brique {
   }
 }
 
+// projectiles[0].crashWith(briques[0][0])
+
 let briques = [
-  [new Brique(110,50), new Brique(250,50), new Brique(390,50), new Brique(530,50), new Brique(670,50), new Brique(810,50), new Brique(950,50), new Brique(1090,50)], // 1ere ligne
-  [new Brique(150,100), new Brique(290,100), new Brique(430,100), new Brique(570,100), new Brique(710,100), new Brique(850,100), new Brique(990,100)], // 2ème ligne
-  [new Brique(110,150), new Brique(250,150), new Brique(390,150), new Brique(530,150), new Brique(670,150), new Brique(810,150), new Brique(950,150), new Brique(1090,150)], // 3ème ligne 
-  [new Brique(150,200), new Brique(290,200), new Brique(430,200), new Brique(570,200), new Brique(710,200), new Brique(850,200), new Brique(990,200)], // 4ème ligne
+  [new Brique(140,50), new Brique(287,50), new Brique(430,50), new Brique(570,50), new Brique(710,50), new Brique(850,50), new Brique(990,50), new Brique(1130,50)], // 1ere ligne
+  [new Brique(205,100), new Brique(353,100), new Brique(495,100), new Brique(635,100), new Brique(775,100), new Brique(915,100), new Brique(1055,100)], // 2ème ligne
+  [new Brique(140,150), new Brique(287,150), new Brique(430,150), new Brique(570,150), new Brique(710,150), new Brique(850,150), new Brique(990,150), new Brique(1130,150)], // 3ème ligne 
+  [new Brique(205,200), new Brique(353,200), new Brique(495,200), new Brique(635,200), new Brique(775,200), new Brique(915,200), new Brique(1055,200)], // 4ème ligne
 
 ] 
 
@@ -57,11 +90,12 @@ let briques = [
 // Faire que la raquette ne sorte pas du CANVAS
 
 
-// quand j'appuie sur touche espace, projectile
+// quand j'appuie sur touche espace, un projectile apparaît
 // tableau de projectile, chaque projectile est un objet avec x, y
 
-class Projectile {
+class Projectile extends Component{
   constructor(x, y, width, height) {
+    super();
     this.x = x;
     this.y = y;
     this.width = width;
@@ -86,54 +120,26 @@ function draw() {
 
   // tout redessiner: les briques + la plateforme + les balles toutes les 16ms
 
-
-  for(let i = 0; i< briques[0].length; i++) {
-    briques[0][i].draw();
+  for (let k = 0; k < briques.length; k++) {
+    for(let i = 0; i< briques[k].length; i++) {
+      if (briques[k][i].on === true) {
+        briques[k][i].draw();
+      }
+      
+    }
+    
   }
 
-  for(let i = 0; i< briques[1].length; i++) {
-    briques[1][i].draw();
-    /*ctx.beginPath();
-    ctx.rect(180 + i*150, 100, width, heigth);
-    ctx.fillStyle = "#8B008B";
-    ctx.fill();
-    ctx.closePath(); */
-  }
+  
 
-  for(let i = 0; i< briques[2].length; i++) {
-    briques[2][i].draw();
-    /*ctx.beginPath();
-    ctx.rect(110 + i*150, 150, width, heigth);
-    ctx.fillStyle = "#A10684";
-    ctx.fill();
-    ctx.closePath(); */
-  }
-
-  for(let i = 0; i< briques[3].length; i++) {
-    briques[3][i].draw();
-    /*ctx.beginPath();
-    ctx.rect(180 + i*150, 200, width, heigth);
-    ctx.fillStyle = "#BD33A4";
-    ctx.fill();
-    ctx.closePath();*/
-  }
+  
 
   // Dessin de la plateforme 
   ctx.beginPath();
   ctx.rect(plateformX, plateformY, plateformWidth, plateformHeigth);
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "rgb(21, 96, 189)";
   ctx.fill();
   ctx.closePath();
-
-  // Dessiner la balle
-
-  /*ctx.beginPath();
-  ctx.arc(Ballx, Bally, diametreBall, 0, Math.PI*2);
-  ctx.fillStyle = "#4120b2";
-  ctx.fill();
-  ctx.closePath();
-  //Ballx += dx;
-  //Bally += dy; */
 
   // Tracer les projectiles
 
@@ -142,27 +148,43 @@ function draw() {
     el.moveUp();
   })
   
-// Faire rebondir la balle pour ne pas qu'elle sorte du CANVAS
+  // collisions
+  // Pour chaque projectile, regarder si il est en collision avec une des briques
+  // si oui, on efface le projectile et la brique du tableau projectile et brique
+  // boucle, crashwidth
 
- /* // Si la valeur x de la position de la balle est inférieure à zéro, on change la direction.
- if (Ballx + dx < 0) {
-  dx = -dx;
-}
 
-// Si la position en x de la balle est supérieure à la largeur du canvas, on inverse encore la vitesse de la balle.
-if(Ballx + dx > canvas.width) {
-  dx = -dx;
-}
+  // pour chq projectile
+  for (let i=0;i<projectiles.length;i++){
+    // pour chq ligne
+    for (let k = 0; k < briques.length; k++) {
+      // pour chq brique
+      for(let j=0; j<briques[k].length;j++) {
+        if (projectiles[i].crashWith(briques[k][j])) {
+          briques[k][j].on = false;
+        }
+      }
+    }
+    
+  }
 
-// Si la valeur y de la position de la balle < 0, on change la direction.
-if(Bally + dy < 0) {
-  dy = -dy;
-}
+  // qd toutes les briques ont la propriete on false
 
-// Si la position de y est > à la hauteur du canvas, on inverse la direction.
-if(Bally + dy > canvas.height) {
-    dy = -dy;
-} */
+  let sum = 0
+  for (let k = 0; k < briques.length; k++) {
+    // pour chq ligne
+    for(let j=0; j<briques[k].length;j++) {
+      // compte le nombre de brique dans la propriété on est false
+      // si j'ai 30 briques  dont la propriété est à false c'est gagné
+      if(briques[k][j].on === false){
+        sum ++;
+      }
+    }
+  }
+  // si sum = 30, alors j'ai gagné
+  if(sum===30){
+    return alert("YOU ARE THE BEST");
+  }
 }
 
 setInterval(draw, 16);
@@ -183,10 +205,8 @@ function keyDownTouch(e) {
       plateformX -= 40;
   }
   if(e.key === " ") { 
-    console.log("coucou");
     const p1 =  new Projectile(plateformX + plateformWidth/2, plateformY - 20, 20, 20) // {x: 250, y: 300, width: 50, height:50}
-    p1.draw()
-    console.log(p1);
+    p1.draw();
     projectiles.push(p1);
   }
 
